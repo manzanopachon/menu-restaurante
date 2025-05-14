db4327285734dbb673f09635893ca927a3ad41b8
manzanopachon <155971032+manzanopachon@users.noreply.github.com>
2025-05-14 15:32:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ async function cargarRestaurante() {
     return;
   }
   try {
-    const res = await fetch(`${API_URL}/restaurantes/${id}`);
+    const res = await fetch(`${API_URL}/restaurantes/${restauranteId}`);
     if (!res.ok) throw new Error(res.statusText);
     const data = await res.json();
     console.log("REST:", data);
