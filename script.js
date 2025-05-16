70ea4cbdec0cb0a4e670de5ece39ea8a6cb17b56
manzanopachon <155971032+manzanopachon@users.noreply.github.com>
2025-05-16 13:59:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://sw-mauri.onrender.com/api";
 
 document.addEventListener("DOMContentLoaded", () => {
   if (window.location.pathname.includes("index.html")) cargarPlatos();
